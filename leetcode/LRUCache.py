2d2189b45a037edbc663bba71e6dc111a06f5f45
caoxudong <xdcao@rongxintx.com>
2015-01-23 16:44:14 +0800

    fix LRUCache

--- leetcode/LRUCache.py
+++ leetcode/LRUCache.py
@@ -20,47 +20,105 @@ class Node:
         self.prev = prev
         self.next = next
 
+class LinkedList:
+    head = None
+    tail = None
+    size = 0
+
+    def __init__(self):
+        pass
+
+    def len(self):
+        return self.size
+
+    def remove(self, node=None):
+        if node == None:
+            return removeFirst()
+        else:
+            if node == self.head:
+                self.head = node.next
+                node.next = None
+            elif node == self.tail:
+                self.tail = node.prev
+                node.prev = None
+            else:
+                node.prev.next = node.next
+                node.next.prev = node.prev    
+            node.next = None
+            node.prev = None
+            self.size = self.size - 1
+            return node
+
+    def removeFirst(self):
+        if self.size == 0:
+            return None
+        else:            
+            if self.size == 1:
+                temp_node = self.head
+                temp_node.next = None
+                self.head = None
+                self.tail = None
+                self.size = self.size - 1
+            else:
+                temp_node = self.head
+                self.head = temp_node.next
+                self.head.prev = None
+                temp_node.prev = None
+                temp_node.next = None
+                self.size = self.size - 1
+            return temp_node
+
+    def append(self, node):
+        if self.size == 0:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+        self.size = self.size + 1
+
 class LRUCache:
  
-    __hash_table = {}
+    hash_table = {}
     __max_size = 0
-    __queue = []
+    queue = LinkedList()
  
     def __init__(self, max_size):
         self.__max_size = max_size
  
     def get(self, key):
-        value_node = self.__hash_table.get(key)
+        value_node = self.hash_table.get(key)
         if value_node is None:
-            return None
+            return -1
         else:
-            if self.__queue.size == 1:
+            if self.queue.len() == 1:
                 pass
             else:
-                value_node.prev.next = value_node.next
-                value_node.next.prev = value_node.prev
-                self.__queue.enqueue(value_node)
+                temp_node = self.queue.remove(value_node)                
+                self.queue.append(temp_node)
             
             return value_node.value
  
     def set(self, key, value):
-        if len(self.__queue) == self.__max_size:
-            node = self.__queue.pop()
-            del self.__hash_table[node.key]
-        new_node = Node(key, value, len(self.__queue))
-        self.__queue.insert(0, new_node)
-        self.__hash_table[key] = new_node
+        value_node = self.hash_table.get(key)
+        if value_node == None:
+            if self.queue.len() == self.__max_size:
+                node = self.queue.removeFirst()
+                del self.hash_table[node.key]
+            new_node = Node(key, value)
+            self.queue.append(new_node)
+            self.hash_table[key] = new_node            
+        else:
+            value_node.value = value
+            value_node = self.queue.remove(value_node)
+            self.queue.append(value_node)
 
-
-
-cache = LRUCache(3)
-cache.set(2, 1)
+cache = LRUCache(2)
 print(cache.get(2))
-cache.set(3, 1)
-cache.set(4, 1)
-cache.set(5, 1)
-cache.set(6, 1)
-print(cache.get(3))
-print(cache.get(4))
-print(cache.get(5))
-print(cache.get(6))
+cache.set(2,6)
+print(cache.get(1))
+cache.set(1,5)
+cache.set(1,2)
+print(cache.get(1))
+print(cache.get(2))
