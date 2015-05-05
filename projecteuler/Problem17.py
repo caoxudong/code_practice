80cb43a0fa44c3c471acaead5c395dc33e84b0c7
caoxudong <caoxudong818@gmail.com>
2015-05-05 10:53:13 +0800

    modify project euler problem17

--- projecteuler/Problem17.py
+++ projecteuler/Problem17.py
@@ -6,3 +6,18 @@ If all the numbers from 1 to 1000 (one thousand) inclusive were written out in w
 
 NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
 '''
+
+import sys
+
+words_count = {}
+words_count['one'] = 3
+words_count['two'] = 3
+words_count['three'] = 5
+words_count['four'] = 4
+words_count['five'] = 4
+words_count['six'] = 6
+words_count['seven'] = 7
+words_count['eight'] = 5
+words_count['night'] = 5
+
+
