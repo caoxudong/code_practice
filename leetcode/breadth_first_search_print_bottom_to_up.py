835cdff31cc052e0aefd0fec2cbeaa9a30b22b47
caoxudong <caoxudong818@gmail.com>
2015-06-30 00:07:29 +0800

    finish Binary Tree Level Order Traversal II

--- leetcode/breadth_first_search_print_bottom_to_up.py
+++ leetcode/breadth_first_search_print_bottom_to_up.py
@@ -7,57 +7,34 @@
 https://oj.leetcode.com/problems/binary-tree-level-order-traversal-ii/
 """
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
-from datastructure.common_datastructure import node
+class Solution:
+    # @param {TreeNode} root
+    # @return {integer[][]}
+    def levelOrderBottom(self, root):
+      result = self.levelOrder(root);
+      return result[::-1]
 
-def visit_node(node):
-  print(node.data)
-
-def visit_list(list):
-  for node in list:
-    visit_node(node)
-
-def breadth_first_search_ii(root=node.Node):
-  if root:
-    queue = []
-    queue.append(root)
-    levels_nodes_queue = []
-
-    while len(queue):
-      current_level_nodes_queue = []
-
-      while len(queue):
-        temp_node = queue.pop(0)
-        current_level_nodes_queue.append(temp_node)
-
-      levels_nodes_queue.insert(0, current_level_nodes_queue)
-
-      for temp_node in current_level_nodes_queue:
-        if temp_node.children and len(temp_node.children) > 0:
-          for child_node in temp_node.children:
-            queue.append(child_node)
-
-    for temp_queue in levels_nodes_queue:
-      visit_list(temp_queue)
-
-if __name__ == '__main__':
-  root = node.Node()
-  root.data = 1
-
-  left = node.Node()
-  left.data = 2
-  root.children.append(left)
-
-  leftleft = node.Node()
-  leftleft.data = 3
-  left.children.append(leftleft)
-
-  leftright = node.Node()
-  leftright.data = 4
-  left.children.append(leftright)
-
-  right = node.Node()
-  right.data = 5
-  root.children.append(right)
-
-  breadth_first_search_ii(root)
+    def levelOrder(self, root):
+      result = []
+      if root:
+        queue = []
+        queue.append(root)
+        while len(queue):
+          level_queue = []
+          next_level_queue = []
+          for node in queue:
+            level_queue.append(node.val)
+            if node.left:
+              next_level_queue.append(node.left)
+            if node.right:
+              next_level_queue.append(node.right)
+          result.append(level_queue)
+          queue = next_level_queue
+      return result
