63988e45d0982bced370f64a51f9020c1e031a54
caoxudong <caoxudong818@gmail.com>
2015-08-04 17:55:04 +0800

    add problem

---
+++ leetcode/max_points_on_a_line.py
@@ -0,0 +1,21 @@
+"""
+
+Max Points on a Line 
+
+Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+
+https://leetcode.com/problems/max-points-on-a-line/
+
+"""
+
+# Definition for a point.
+# class Point:
+#     def __init__(self, a=0, b=0):
+#         self.x = a
+#         self.y = b
+
+class Solution:
+    # @param {Point[]} points
+    # @return {integer}
+    def maxPoints(self, points):
+        
