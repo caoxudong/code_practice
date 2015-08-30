7885a6b34ff9040dd8ebd81e417ec53122cce703
caoxudong <caoxudong818@gmail.com>
2015-08-30 14:53:02 +0800

    finish one problem

--- leetcode/max_points_on_a_line.py
+++ leetcode/max_points_on_a_line.py
@@ -15,7 +15,33 @@ https://leetcode.com/problems/max-points-on-a-line/
 #         self.y = b
 
 class Solution:
-    # @param {Point[]} points
-    # @return {integer}
     def maxPoints(self, points):
+      """
+      :type points: List[Point]
+      :rtype: int
+      """
+      length = len(points)
+      if length < 3: 
+        return length
+      result = -1
+      for i in range(length):
+        slope = {'inf': 0}
+        sameNum = 1
+
+        for j in range(length):
+          if i == j:
+            continue
+          elif points[i].x == points[j].x and points[i].y != points[j].y:
+            slope['inf'] += 1
+          elif points[i].x != points[j].x:
+            k = 1.0 * (points[i].y - points[j].y) / (points[i].x - points[j].x)
+            if k not in slope:
+              slope[k] = 1
+            else:
+              slope[k] += 1
+          else:
+            sameNum += 1
+        result = max(result, max(slope.values()) + sameNum)
+      return result
+
         
