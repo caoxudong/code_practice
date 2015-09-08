0150f2dfaf744430cdc2510cc2f965665f184c1e
caoxudong <caoxudong818@gmail.com>
2015-09-08 14:37:58 +0800

    finish first_bad_version

--- leetcode/first_bad_version.py
+++ leetcode/first_bad_version.py
@@ -8,6 +8,8 @@ You are a product manager and currently leading a team to develop a new product.
 Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
 
 You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+https://leetcode.com/problems/first-bad-version/
 """
 
 # The isBadVersion API is already defined for you.
