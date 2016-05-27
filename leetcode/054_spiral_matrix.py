d1b08c24af0ebdb1752851f643a66237e38130db
caoxudong <caoxudong818@gmail.com>
2016-05-27 17:03:23 +0800

    add problem leetcode 054

---
+++ leetcode/054_spiral_matrix.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+"""
+https://leetcode.com/problems/spiral-matrix/
+
+
+Given a matrix of m * n elements (m rows, n columns), return all elements of the matrix in spiral order.
+
+For example,
+Given the following matrix:
+
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+
+You should return [1,2,3,6,9,8,7,4,5].
+"""
+
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
