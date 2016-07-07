ccb1cdfd9e75bfd838ac4f23952695694bcee3fb
caoxudong <caoxudong818@gmail.com>
2016-07-08 00:00:21 +0800

    update problem

--- leetcode/138_copy_list_with_random_pointer.py
+++ leetcode/138_copy_list_with_random_pointer.py
@@ -31,7 +31,7 @@ class Solution(object):
         # put the new copied node next to it's original node,
         step_node = head
         while step_node != None:
-            new_node = RandomListNode(step_node.label)
+            new_node = RandomListNode(step_node.label * 10)
             new_node.next = step_node.next
             step_node.next = new_node
             step_node = new_node.next
@@ -40,15 +40,15 @@ class Solution(object):
         step_node = head
         while step_node != None:
             target_node = step_node.next
-            target_node.random = step_node.random
+            target_node.random = step_node.random and step_node.random.next
             step_node = target_node.next
         
         # delete origin node
         step_node, copied_node, result = head, head.next, head.next
-        while step_node != None:
-            step_node = copied_node.next
-            copied_node.next = step_node.next
-            copied_node = step_node.next
+        while copied_node != None:            
+            step_node.next = copied_node.next
+            copied_node = step_node.next and step_node.next.next
+            step_node = step_node.next
 
         return result
 
@@ -67,7 +67,7 @@ def printList(head):
 head = RandomListNode(0)
 node_list = []
 step_node = head
-for i in range(1, 5):
+for i in range(1, 10):
     temp_node = RandomListNode(i)
     step_node.next = temp_node
     step_node = temp_node
@@ -76,7 +76,14 @@ for i in range(1, 5):
 for temp_node in node_list:
     temp_node.random = node_list[random.randint(0, len(node_list)) - 1]
 
+printList(head)
+
+print('================')
+
 solution = Solution()
 copied_list = solution.copyRandomList(head)
 printList(copied_list)        
 
+
+print('================')
+printList(head)
