954e7b422fd0be0f2836bca39c14b98777cde98b
caoxudong <caoxudong818@gmail.com>
2016-07-08 11:01:28 +0800

    finish leetcode 138

--- leetcode/138_copy_list_with_random_pointer.py
+++ leetcode/138_copy_list_with_random_pointer.py
@@ -45,12 +45,14 @@ class Solution(object):
         
         # delete origin node
         step_node, copied_node, result = head, head.next, head.next
-        while copied_node != None:            
+        while step_node != None:
             step_node.next = copied_node.next
-            copied_node = step_node.next and step_node.next.next
-            step_node = step_node.next
+            step_node = copied_node.next
+            copied_node.next = step_node and step_node.next
+            copied_node = step_node and step_node.next 
 
         return result
+
 
 
 
@@ -67,7 +69,11 @@ def printList(head):
 head = RandomListNode(0)
 node_list = []
 step_node = head
+<<<<<<< ccb1cdfd9e75bfd838ac4f23952695694bcee3fb
 for i in range(1, 10):
+=======
+for i in range(1, 2):
+>>>>>>> finish leetcode 138
     temp_node = RandomListNode(i)
     step_node.next = temp_node
     step_node = temp_node
@@ -76,6 +82,7 @@ for i in range(1, 10):
 for temp_node in node_list:
     temp_node.random = node_list[random.randint(0, len(node_list)) - 1]
 
+<<<<<<< ccb1cdfd9e75bfd838ac4f23952695694bcee3fb
 printList(head)
 
 print('================')
@@ -87,3 +94,14 @@ printList(copied_list)
 
 print('================')
 printList(head)
+=======
+head = RandomListNode(-1)
+
+printList(head)
+print("=================")
+solution = Solution()
+copied_list = solution.copyRandomList(head)
+printList(copied_list)        
+print("=================")
+printList(head)
+>>>>>>> finish leetcode 138
