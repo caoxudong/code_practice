ae62b0df187da38607d628f6b12cd82b2235febe
caoxudong <caoxudong@didichuxing.com>
2018-03-08 14:25:20 +0800

    finish leetcode 003

---
+++ leetcode/003_longest_substring_without_repeating_characters.py
@@ -0,0 +1,36 @@
+"""
+
+Given a string, find the length of the longest substring without repeating characters.
+
+Examples:
+
+Given "abcabcbb", the answer is "abc", which the length is 3.
+
+Given "bbbbb", the answer is "b", with the length of 1.
+
+Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+"""
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        chars_indexes = {}
+        begin_index = 0
+        max_length = 0
+        for i, char in enumerate(s):
+            if char in chars_indexes and begin_index <= chars_indexes[char] :
+                begin_index = chars_indexes[char] + 1
+            else :
+                max_length = max(max_length, i - begin_index + 1)
+            chars_indexes[char] = i
+        return max_length
+
+if __name__ == '__main__':
+    obj = Solution()
+    strings = ['abcdefd', 'abcabcbb', 'bbbb', 'pwwkew']
+    for string in strings:
+        print(obj.lengthOfLongestSubstring(string))
--- leetcode/133_clone_graph.py
+++ leetcode/133_clone_graph.py
@@ -35,10 +35,10 @@ import os
 import random
 
 # Definition for a undirected graph node
-# class UndirectedGraphNode(object):
-#     def __init__(self, x):
-#         self.label = x
-#         self.neighbors = []
+class UndirectedGraphNode(object):
+    def __init__(self, x):
+        self.label = x
+        self.neighbors = []
     
 class Solution:
     # @param node, a undirected graph node
