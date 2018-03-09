f460cbe9b7e8f7a16a16288321cecdd15c53ffbb
caoxudong <caoxudong@didichuxing.com>
2018-03-09 14:18:24 +0800

    add leetcode problem 005

---
+++ leetcode/005_longest_palindromic_substring.py
@@ -0,0 +1,26 @@
+"""
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example:
+
+Input: "babad"
+
+Output: "bab"
+
+Note: "aba" is also a valid answer.
+ 
+
+Example:
+
+Input: "cbbd"
+
+Output: "bb"
+"""
+
+class Solution:
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
