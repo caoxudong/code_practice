2f7210d089083a04b3c347c1caf4cdec61a50b23
caoxudong <caoxudong818@gmail.com>
2020-03-19 19:49:46 +0800

    mod comments

--- leetcode/003_longest_substring_without_repeating_characters.py
+++ leetcode/003_longest_substring_without_repeating_characters.py
@@ -1,4 +1,5 @@
 """
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
 Given a string, find the length of the longest substring without repeating characters.
 
--- leetcode/005_longest_palindromic_substring.py
+++ leetcode/005_longest_palindromic_substring.py
@@ -1,4 +1,6 @@
 """
+https://leetcode.com/problems/longest-palindromic-substring/
+
 Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
 
 Example:
