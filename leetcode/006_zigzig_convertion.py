03aadafce3fc670db469dd350e34d68cf115a584
caoxudong <caoxudong818@gmail.com>
2020-03-19 19:51:13 +0800

    mod comment

--- leetcode/006_zigzig_convertion.py
+++ leetcode/006_zigzig_convertion.py
@@ -12,13 +12,13 @@ Write the code that will take a string and make this conversion given a number o
 
 string convert(string s, int numRows);
 Example 1:
-
 Input: s = "PAYPALISHIRING", numRows = 3
 Output: "PAHNAPLSIIGYIR"
-Example 2:
 
+Example 2:
 Input: s = "PAYPALISHIRING", numRows = 4
 Output: "PINALSIGYAHRPI"
+
 Explanation:
 
 P     I    N
