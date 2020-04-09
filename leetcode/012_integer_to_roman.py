53dc083cb78a84e16257b703c0cb68ebb396020e
caoxudong <caoxudong818@gmail.com>
2020-04-09 21:59:16 +0800

    finish problem

--- leetcode/012_integer_to_roman.py
+++ leetcode/012_integer_to_roman.py
@@ -17,6 +17,7 @@ Roman numerals are usually written largest to smallest from left to right. Howev
 I can be placed before V (5) and X (10) to make 4 and 9. 
 X can be placed before L (50) and C (100) to make 40 and 90. 
 C can be placed before D (500) and M (1000) to make 400 and 900.
+
 Given an integer, convert it to a roman numeral. Input is guaranteed to be within the range from 1 to 3999.
 
 Example 1:
@@ -42,6 +43,56 @@ Output: "MCMXCIV"
 Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 """
 
+
 class Solution:
     def intToRoman(self, num: int) -> str:
-        pass
+        roman_numerals = [
+            (1000, 'M', None),
+            (100,  'C', {9: 'CM', 8: 'DCCC',
+                         7: 'DCC', 6: 'DC', 5: 'D', 4: 'CD'}),
+            (10,   'X', {9: 'XC', 8: 'LXXX',
+                         7: 'LXX', 6: 'LX', 5: 'L', 4: 'XL'}),
+            (1,    'I', {9: 'IX', 8: 'VIII',
+                         7: 'VII', 6: 'VI', 5: 'V', 4: 'IV'})
+        ]
+        roman_numerals_len = len(roman_numerals)
+
+        result_numerals = []
+        roman_numerals_index = 0
+        remainder = num
+        while True:
+            if roman_numerals_index >= roman_numerals_len:
+                break
+            quotient = remainder // roman_numerals[roman_numerals_index][0]
+            if quotient > 0:
+                if roman_numerals[roman_numerals_index][2] is None:
+                    result_numerals.append(
+                        roman_numerals[roman_numerals_index][1] * quotient)
+                else:
+                    if roman_numerals[roman_numerals_index][2].get(quotient) is None:
+                        result_numerals.append(
+                            roman_numerals[roman_numerals_index][1] * quotient)
+                    else:
+                        result_numerals.append(
+                            roman_numerals[roman_numerals_index][2][quotient])
+            remainder = remainder % roman_numerals[roman_numerals_index][0]
+            roman_numerals_index += 1
+
+        result = "".join(result_numerals)
+        return result
+
+
+if __name__ == "__main__":
+    test_params = [
+        (3, "III"),
+        (4, "IV"),
+        (9, "IX"),
+        (58, "LVIII"),
+        (1994, "MCMXCIV"),
+        (60, "LX")
+    ]
+    s = Solution()
+    for test in test_params:
+        result = s.intToRoman(test[0])
+        print((test, result))
+        assert test[1] == s.intToRoman(test[0])
