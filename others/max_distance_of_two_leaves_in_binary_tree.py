7f92db1517099ffc28001ced36b0e5e59b5948b2
caoxudong <caoxudong818@gmail.com>
2020-04-18 18:20:34 +0800

    add trees

---
+++ others/max_distance_of_two_leaves_in_binary_tree.py
@@ -0,0 +1,12 @@
+"""
+找出二叉树中，叶子节点最大的距离。
+"""
+
+class Node:
+    def __init__(self, left, right, value):
+        self.left = left
+        self.right = right
+        self.value = value
+
+def max_distance(node):
+    pass
