62fd6f3aac6f25ae205b1655ffecc1f0b6c27b5b
caoxudong <caoxudong818@gmail.com>
2020-04-20 12:33:16 +0800

    add problem

--- leetcode/15_3sum.py
+++ leetcode/15_3sum.py
@@ -16,6 +16,39 @@ A solution set is:
 ]
 """
 
+
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        pass
+    def threeSum(self, nums):
+        nums = sorted(nums)
+        nums_count = len(nums)
+        result = []
+        for i in range(nums_count - 2):
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            j = i + 1
+            k = nums_count - 1
+
+            while j < k:
+                sum_result = nums[i] + nums[j] + nums[k]
+                if sum_result == 0:
+                    result.append([nums[i], nums[j], nums[k]])
+                    while j < k and nums[j] == nums[j + 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k - 1]:
+                        k -= 1
+                    j += 1
+                    k -= 1
+                elif sum_result > 0:
+                    k -= 1
+                else:
+                    j += 1
+        return result
+
+
+if __name__ == "__main__":
+    params = [-1, 0, 1, 2, -1, -4]
+    s = Solution()
+    print(s.threeSum(params))
