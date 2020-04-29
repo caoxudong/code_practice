ddd02e6cffbb87422690e56ff68945d161790090
caoxudong <caoxudong818@gmail.com>
2020-04-29 12:13:44 +0800

    finish problem

--- leetcode/300_longest_increasing_subsequence.py
+++ leetcode/300_longest_increasing_subsequence.py
@@ -13,5 +13,34 @@ Your algorithm should run in O(n2) complexity.
 """
 
 class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
-        pass
+    def innerLengthOfLIS(self, nums, prev_pos, cur_pos, middle_result):
+        if cur_pos == len(nums):
+            return 0
+
+        middle_result_value = middle_result[prev_pos + 1][cur_pos]
+        if middle_result_value >= 0:
+            return middle_result_value
+
+        including = 0
+        if prev_pos < 0 or nums[cur_pos] > nums[prev_pos]:
+                including = self.innerLengthOfLIS(nums, cur_pos, cur_pos + 1, middle_result) + 1
+
+        not_including = self.innerLengthOfLIS(nums, prev_pos, cur_pos + 1, middle_result)
+        middle_result_value = max(including, not_including)
+        middle_result[prev_pos + 1][cur_pos] = middle_result_value
+
+        return middle_result_value
+
+    def lengthOfLIS(self, nums):
+        middle_result = [[-1] * len(nums) for i in range(len(nums) + 1)]
+        return self.innerLengthOfLIS(nums, -1, 0, middle_result)
+
+if __name__ == "__main__":
+    tests = [
+        # ([10,9,2,5,3,7,101,18], 4),
+        # ([4,10,4,3,8,9], 3),
+        ([10,9,2,5,3,4], 3)
+    ]
+    s = Solution()
+    for t in tests:
+        print(t[0], t[1], s.lengthOfLIS(t[0]))
