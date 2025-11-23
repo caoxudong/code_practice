f5a9ed62688869a42d8845c0da12f0daa3639ace
caoxudong <caoxudong818@gmail.com>
2025-11-23 17:10:12 +0800

    add leetcode problem 32

---
+++ leetcode/problem_32_longest_valid_parentheses_test.py
@@ -0,0 +1,23 @@
+import unittest
+import leetcode.problem_32_longest_valid_parentheses as problem
+
+
+class UnitTestData:
+    def __init__(self, s: str = "", expected: int = 0):
+        self.s = s
+        self.expected = expected
+
+
+unittest_data = [
+    UnitTestData(s="", expected=0),
+    UnitTestData(s="(()", expected=2),
+    UnitTestData(s=")()())", expected=4),
+]
+
+
+class TestSolution(unittest.TestCase):
+    def test_longestValidParentheses(self):
+        s = problem.Solution()
+        for item in unittest_data:
+            result = s.longestValidParentheses(item.s)
+            self.assertEqual(result, item.expected)
