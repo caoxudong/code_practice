01b9d28ab3d2e11c6574138204ef3c0214e521e2
caoxudong <caoxudong818@gmail.com>
2025-12-15 21:49:21 +0800

    add leetcode problem 37

---
+++ leetcode/problem_37_sudoku_solver_test.py
@@ -0,0 +1,47 @@
+import unittest
+import leetcode.problem_37_sudoku_solver as problem
+
+
+class UnitTestData:
+    def __init__(
+        self, board: list[list[str]] = [], expected: list[list[int]] = []
+    ) -> bool:
+        self.board = board
+        self.expected = expected
+
+
+unittest_data = [
+    UnitTestData(
+        board=[
+            ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+            ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+            [".", "9", "8", ".", ".", ".", ".", "6", "."],
+            ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+            ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+            ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+            [".", "6", ".", ".", ".", ".", "2", "8", "."],
+            [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+            [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+        ],
+        expected=[
+            ["5", "3", "4", "6", "7", "8", "9", "1", "2"],
+            ["6", "7", "2", "1", "9", "5", "3", "4", "8"],
+            ["1", "9", "8", "3", "4", "2", "5", "6", "7"],
+            ["8", "5", "9", "7", "6", "1", "4", "2", "3"],
+            ["4", "2", "6", "8", "5", "3", "7", "9", "1"],
+            ["7", "1", "3", "9", "2", "4", "8", "5", "6"],
+            ["9", "6", "1", "5", "3", "7", "2", "8", "4"],
+            ["2", "8", "7", "4", "1", "9", "6", "3", "5"],
+            ["3", "4", "5", "2", "8", "6", "1", "7", "9"],
+        ],
+    ),
+]
+
+
+class TestSolution(unittest.TestCase):
+    def test_solveSudoku(self):
+        s = problem.Solution()
+        for item in unittest_data:
+            s.solveSudoku(item.board)
+            for i in range(9):
+                self.assertListEqual(item.board[i], item.expected[i])
