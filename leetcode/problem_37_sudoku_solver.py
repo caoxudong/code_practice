371d7f5e360c93f0f09b7597009dd7ead544ad80
caoxudong <caoxudong818@gmail.com>
2026-07-21 22:01:41 +0800

    add leetcode 37 implmentation

--- leetcode/problem_37_sudoku_solver.py
+++ leetcode/problem_37_sudoku_solver.py
@@ -50,4 +50,33 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        pass
+
+        def validate(board: list[list[str]], i: int, j: int, n: int) -> bool:
+            c = "%s"%(n+1)
+            for a in range(0,9):
+                if c == board[i][a] or c == board[a][j]:
+                    return False
+            for a in range(0,3):
+                for b in range(0,3):
+                    if board[int(i/3)*3+a][int(j/3)*3+b] == c:
+                        return False
+            return True
+
+        def solveSudokuInner(board: list[list[str]]) -> bool:
+            for i in range(0,9):
+                for j in range(0,9):
+                    c = board[i][j]
+                    if c == ".":
+                        for n in range (0, 9):
+                            if validate(board, i, j, n):
+                                board[i][j] = "%s"%(n+1)
+                                if solveSudokuInner(board):
+                                    return True
+                                board[i][j] = "."
+                        return False
+            return True
+        
+        solveSudokuInner(board)
+        
+
+                
