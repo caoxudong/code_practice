4dfd023d4c9302803f9beadabaa0f25378faf735
caoxudong <caoxudong818@gmail.com>
2026-07-24 21:25:36 +0800

    add leecode problem 42

---
+++ leetcode/problem_42_trapping_rain_water_test.py
@@ -0,0 +1,23 @@
+from typing import List
+import unittest
+import leetcode.problem_41_first_missing_positive as problem
+
+
+class UnitTestData:
+    def __init__(self, height: List[int], expected: int) -> None:
+        self.height = height
+        self.expected = expected
+
+
+unittest_data = [
+    UnitTestData(height=[0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], expected=6),
+    UnitTestData(height=[4, 2, 0, 3, 2, 5], expected=9),
+]
+
+
+class TestSolution(unittest.TestCase):
+    def test_leetcode_42_trap(self):
+        s = problem.Solution()
+        for item in unittest_data:
+            retval = s.trap(item.height)
+            self.assertEqual(retval, item.expected)
