6d29c3d7247844e680e7288f9f38b1ac5d208e3d
caoxudong <caoxudong818@gmail.com>
2026-08-08 23:29:24 +0800

    add leetcode 44

---
+++ leetcode/problem_44_wildcard_matching_test.py
@@ -0,0 +1,25 @@
+from typing import List
+import unittest
+import leetcode.problem_44_wildcard_matching as problem
+
+
+class UnitTestData:
+    def __init__(self, s: str, p: str, expected: bool) -> None:
+        self.s = s
+        self.p = p
+        self.expected = expected
+
+
+unittest_data = [
+    UnitTestData(s="aa", p="a", expected=False),
+    UnitTestData(s="aa", p="*", expected=True),
+    UnitTestData(s="cb", p="?a", expected=False),
+]
+
+
+class TestSolution(unittest.TestCase):
+    def test_leetcode_44_isMatch(self):
+        s = problem.Solution()
+        for item in unittest_data:
+            retval = s.isMatch(item.s, item.p)
+            self.assertEqual(retval, item.expected)
