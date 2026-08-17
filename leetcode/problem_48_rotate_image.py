dd8032dba30f98ba3fa337980b65da68b8562966
caoxudong <caoxudong818@gmail.com>
2026-08-17 21:17:12 +0800

    finish leetcode 48

--- leetcode/problem_48_rotate_image.py
+++ leetcode/problem_48_rotate_image.py
@@ -41,4 +41,12 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        pass
+        n = len(matrix)
+        for i in range(n >> 1):
+            for j in range(i, n - 1 - i):
+                matrix[i][j], matrix[j][~i], matrix[~i][~j], matrix[~j][i] = (
+                    matrix[~j][i],
+                    matrix[i][j],
+                    matrix[j][~i],
+                    matrix[~i][~j],
+                )
