116e6950091c98d4eff60ecfc6dbc4361bdf38fd
caoxudong <caoxudong818@gmail.com>
2026-08-21 15:52:06 +0800

    add leetcode 62

---
+++ leetcode/problem_62_unique_paths_test.py
@@ -0,0 +1,26 @@
+from typing import Optional
+import unittest
+from common_data_structure.list_node import ListNode
+import common_data_structure.list_node as list_node
+import leetcode.problem_62_unique_paths as problem
+
+
+class UnitTestData:
+    def __init__(self, m: int, n: int, expected: int) -> None:
+        self.m = m
+        self.n = n
+        self.expected = expected
+
+
+unittest_data = [
+    UnitTestData(m=3, n=7, expected=28),
+    UnitTestData(m=3, n=2, expected=3),
+]
+
+
+class TestSolution(unittest.TestCase):
+    def test_leetcode_62_uniquePaths(self):
+        s = problem.Solution()
+        for item in unittest_data:
+            retval = s.uniquePaths(item.m, item.n)
+            self.assertEqual(retval, item.expected)
