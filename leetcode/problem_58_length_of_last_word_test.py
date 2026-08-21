ad0bdeb595f3bb1837b982a23be1777eebd42232
caoxudong <caoxudong818@gmail.com>
2026-08-21 11:10:33 +0800

    add leetcode 58

---
+++ leetcode/problem_58_length_of_last_word_test.py
@@ -0,0 +1,24 @@
+from typing import List
+import unittest
+import leetcode.problem_58_length_of_last_word as problem
+
+
+class UnitTestData:
+    def __init__(self, s: str, expected: int) -> None:
+        self.s = s
+        self.expected = expected
+
+
+unittest_data = [
+    UnitTestData(s="Hello World", expected=5),
+    UnitTestData(s="   fly me   to   the moon  ", expected=4),
+    UnitTestData(s="luffy is still joyboy", expected=6),
+]
+
+
+class TestSolution(unittest.TestCase):
+    def test_leetcode_58_lengthOfLastWord(self):
+        s = problem.Solution()
+        for item in unittest_data:
+            retval = s.lengthOfLastWord(item.intervals, item.newInterval)
+            self.assertEqual(item.expected, retval)
