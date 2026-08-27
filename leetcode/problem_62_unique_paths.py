fe2c604ed4d697fb5b9cf7b7b1421499885c56ab
caoxudong <caoxudong818@gmail.com>
2026-08-27 21:52:17 +0800

    upgrade leetcode 62 performance

--- leetcode/problem_62_unique_paths.py
+++ leetcode/problem_62_unique_paths.py
@@ -27,17 +27,12 @@ Constraints:
 * 1 <= m, n <= 100
 """
 
-from functools import cache
+from itertools import product
 
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        @cache
-        def dfs(i, j):
-            if i >= m or j >= n:
-                return 0
-            if i == m - 1 and j == n - 1:
-                return 1
-            return dfs(i + 1, j) + dfs(i, j + 1)
-
-        return dfs(0, 0)
+        dp = [[1] * n for i in range(m)]
+        for i, j in product(range(1, m), range(1, n)):
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[-1][-1]
