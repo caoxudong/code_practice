d9b9c0093f592a42b5bae8f73886f528c0508038
caoxudong <caoxudong818@gmail.com>
2026-09-10 18:50:33 +0800

    finish leetcode 75

--- leetcode/problem_75_set_colors.py
+++ leetcode/problem_75_set_colors.py
@@ -36,4 +36,22 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        pass
+        zero = one = two = 0
+        for n in nums:
+            if n == 0:
+                zero += 1
+            elif n == 1:
+                one += 1
+            else:
+                two += 1
+
+        for i in range(len(nums)):
+            if zero > 0:
+                nums[i] = 0
+                zero -= 1
+            elif one > 0:
+                nums[i] = 1
+                one -= 1
+            else:
+                nums[i] = 2
+                two -= 1
