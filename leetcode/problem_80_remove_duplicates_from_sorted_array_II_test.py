46a01b041c4f96b75e7e4cfc391157d655b97a03
caoxudong <caoxudong818@gmail.com>
2026-09-13 14:20:14 +0800

    add leetcode 80

--- leetcode/problem_80_remove_duplicates_from_sorted_array_II_test.py
+++ leetcode/problem_80_remove_duplicates_from_sorted_array_II_test.py
@@ -14,3 +14,12 @@ unittest_data = [
     UnitTestData(nums=[1, 1, 1, 2, 2, 3], expected_num=5, expected_list=[1, 1, 2, 2, 3, None]),
     UnitTestData(nums=[0, 0, 1, 1, 1, 1, 2, 3, 3], expected_num=7, expected_list=[0, 0, 1, 1, 2, 3, 3, None, None]),
 ]
+
+class TestSolution(unittest.TestCase):
+    def test_leetcode_80_remove_duplicates(self):
+        s = problem.Solution()
+        for item in unittest_data:
+            nums_copy = item.nums.copy()  # Create a copy to avoid modifying the original list
+            retval = s.removeDuplicates(nums_copy)
+            self.assertEqual(retval, item.expected_num)
+            self.assertEqual(nums_copy[:retval], item.expected_list[:retval])
