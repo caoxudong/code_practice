baca0803a2e63da7402a65c8793f840040f1c131
caoxudong <caoxudong818@gmail.com>
2026-09-13 14:19:25 +0800

    add leetcode 80

---
+++ leetcode/problem_80_remove_duplicates_from_sorted_array_II_test.py
@@ -0,0 +1,16 @@
+from typing import List
+import unittest
+import leetcode.problem_80_remove_duplicates_from_sorted_array_II as problem
+
+
+class UnitTestData:
+    def __init__(self, nums: List[int], expected_num: int, expected_list: List[int]):
+        self.nums = nums
+        self.expected_num = expected_num
+        self.expected_list = expected_list
+
+
+unittest_data = [
+    UnitTestData(nums=[1, 1, 1, 2, 2, 3], expected_num=5, expected_list=[1, 1, 2, 2, 3, None]),
+    UnitTestData(nums=[0, 0, 1, 1, 1, 1, 2, 3, 3], expected_num=7, expected_list=[0, 0, 1, 1, 2, 3, 3, None, None]),
+]
