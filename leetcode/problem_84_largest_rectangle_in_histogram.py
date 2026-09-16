ec288feedf94d2f4212cf8f021cca595ef81b700
caoxudong <caoxudong818@gmail.com>
2026-09-16 19:39:16 +0800

    finish leetcode 84

--- leetcode/problem_84_largest_rectangle_in_histogram.py
+++ leetcode/problem_84_largest_rectangle_in_histogram.py
@@ -25,4 +25,29 @@ from typing import List
 
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        return 0
+        n = len(heights)
+        left, right = [-1] * n, [n] * n
+        stack = []
+
+        # Nearest Smaller to Left
+        for i in range(n):
+            while stack and heights[stack[-1]] >= heights[i]:
+                stack.pop()
+            left[i] = stack[-1] if stack else -1
+            stack.append(i)
+
+        stack.clear()
+
+        # Nearest Smaller to Right
+        for i in range(n - 1, -1, -1):
+            while stack and heights[stack[-1]] >= heights[i]:
+                stack.pop()
+            right[i] = stack[-1] if stack else n
+            stack.append(i)
+
+        max_area = 0
+        for i in range(n):
+            width = right[i] - left[i] - 1
+            max_area = max(max_area, heights[i] * width)
+
+        return max_area
