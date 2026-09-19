258e2440aeeed47d75d64169e8e371f0ae261138
caoxudong <caoxudong818@gmail.com>
2026-09-19 18:16:06 +0800

    finish leetcode 85

--- leetcode/problem_85_maximal_rectangle.py
+++ leetcode/problem_85_maximal_rectangle.py
@@ -39,16 +39,12 @@ class Solution:
                 heights[i] = heights[i] + 1 if row[i] == "1" else 0
 
             # Calculate max area using histogram method
-            n = len(heights)  # Number of bars in the histogram
-
-            for i in range(n):
-                for j in range(i, n):
-                    # Determine the minimum height between bar i and bar j
-                    min_height = min(heights[k] for k in range(i, j + 1))
-                    # Calculate the area of the rectangle
-                    area = min_height * (j - i + 1)
-                    # Update maximum area if the current rectangle's area is larger
-                    if area > max_area:
-                        max_area = area
+            stack = []
+            for i in range(len(heights)):
+                while stack and heights[i] < heights[stack[-1]]:
+                    h = heights[stack.pop()]
+                    w = i if not stack else i - stack[-1] - 1
+                    max_area = max(max_area, h * w)
+                stack.append(i)
 
         return max_area
